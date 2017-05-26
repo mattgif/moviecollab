12b363a3afca4d501ef6f6f3571ca719fbe682d1
Matt Gifford <mattgif@gmail.com>
2017-05-26 09:03:07 -0400

    moviecollab, removed deprecated imports

--- collabfinder/views.py
+++ collabfinder/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
-import secrets
-
-api_key = secrets.my_api_key()
 
 # Create your views here.
 def index(request):
